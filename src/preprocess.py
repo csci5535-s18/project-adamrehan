10b23d74ec2a66883067c44c5b09e0e37af96104
rehan <ahmeshaf.rehan@gmail.com>
2018-04-28 17:08:55 -0600

    updated pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,3 +1,12 @@
 '''
 Major thing to do here is to fill in the missing containers in the text through co-ref resolver, etc.
 '''
+
+def preprocess(anlp, text):
+    '''
+    Todo: coref
+    :param anlp:
+    :param text:
+    :return:
+    '''
+    return ['Pooja has 3 apples', 'Pooja eats 1 apple', 'How many apples does Pooja have now?']
