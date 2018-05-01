4f63385dba93c24aa9352190092b77103021e6bb
adam <ajwieme@gmail.com>
2018-05-01 01:09:38 -0600

    test script for classification

--- src/test_sentence_classifier.py
+++ src/test_sentence_classifier.py
@@ -2,4 +2,20 @@ from sentence_classifier import VerbClassifier
 
 classifier = VerbClassifier()
 
-print(classifier.classify(["construct", "destroy"], ["eats"]))
+
+pred2label = {"destroy": "d", "construct": "c"}
+acc = 0
+total = 0
+for label, verbs in classifier.labeled_verbs.items():
+    for v in verbs:
+        pred = classifier.classify(["construct", "destroy"], [v])[0]
+        target = label
+        print(v)
+        print("guess: %s" % pred)
+        print("correct: %s" % label)
+        if pred2label[pred] == label:
+            acc += 1
+            
+        total += 1
+
+print("ACC: %2f" % (acc / total))
