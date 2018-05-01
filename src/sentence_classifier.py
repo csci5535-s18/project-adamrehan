9642a000fa9517eb75b1f4aae978eefb2e6e5c1a
adam <ajwieme@gmail.com>
2018-04-30 18:02:06 -0600

    classifier

--- src/sentence_classifier.py
+++ src/sentence_classifier.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pickle
+import gensim
+
+EMBEDDINGS_FILE ="/home/adam/project-adamrehan/src/embeddings/GoogleNews-vectors-negative300.bin.gz"
+CONSTRUCT_EMBEDDINGS_FILE = "/home/adam/project-adamrehan/src/embeddings/construct_embedding.npy"
+DESTROY_EMBEDDINGS_FILE = "/home/adam/project-adamrehan/src/embeddings/destroy_embedding.npy"
 
 OBSERVATION = 'observation'
 CONS = 'construct'
@@ -8,34 +13,63 @@ NTRANS = 'n_transfer'
 PTRANS = 'p_transfer'
 GET = 'get'
 
-class VerbCategoryClassifier(object):
-    """
-    Classify a Verb Phrase according to the operations in imp
-    """
-    def __init__(self, classnames=None, verbs=None):
-        # self.classnames = classnames
-        # self.n_components = len(classnames)
-        self.verbs = verbs
-    def vectorize(self, sentences):
-        '''
-        getting features out of sentences: using Wordnet, word2vec, Verbnet, etc
-        If self.verbs is None: We'll need to extract the verbs
-        :param sentences:
-        :return:
-        '''
-        pass
-    def extract_verbs(self, sentences):
-        pass
-    def fit(self, X, y):
-        pass
-    def predict(self, X):
-        pass
-    def predict_labels(self, sentences):
-        # Todo:
-        return [OBSERVATION, DESTROY, GET]
-    def fit_predict(self, X):
-        pass
-def load(filename):
-    # TODO:
-    return VerbCategoryClassifier()
-    # return VerbCategoryClassifier(pickle.load(open(filename, 'rb')))
+class VerbClassifier():
+    def __init__(self):
+        # A matrix 
+        self.W = self.loadWordEmbeds(EMBEDDINGS_FILE)
+        self.construct_embed = self.loadLabelEmbeds(CONSTRUCT_EMBEDDINGS_FILE)
+        self.destroy_embed = self.loadLabelEmbeds(DESTROY_EMBEDDINGS_FILE)
+        
+    def get_average_embedding(self, words):
+        embeds = self.W[words]
+        # n x DIMS -> DIMS of average
+        return np.average(embeds, axis=0)
+
+    def get_label_embeds(self, label):
+        if label in [CONS, PTRANS]:
+            return self.construct_embed
+        elif label in [DESTROY, NTRANS]:
+            return self.destroy_embed
+        else:
+            raise Exception("Unexpected labe %s" % label)
+        
+    def classify(self, labels, verb_phrase):
+        """
+        labels: the constrained set of possible labels
+        verb_phrase: a list of the verb and its arguments
+
+        return: a list of the labels ordered by how likely each is
+
+        Because labels are always scoped such that we only have to choose
+        between construct/destroy, and negatie/positive transfer
+        We basically just need to decide if we have an instance of
+        the Agent of the verb gaining or losing something
+        """
+        embed = self.get_average_embedding(verb_phrase)
+        similarity_dict = {l: 0.0 for l in labels}
+        for l in labels:
+            similarity_dict[l] = self._cosine_sim(embed,\
+                                        self.get_label_embeds(l))
+
+        # Return labels in order of how similar their embedding is to
+        # the input verb phrase
+        print(similarity_dict)
+        return sorted(similarity_dict, key=similarity_dict.get)
+
+    def _cosine_sim(self, w1, w2):
+        """
+        Get the cosine similarity between 2 n-dimensional vectors
+
+        dot-product / product of norms
+        """
+        return np.dot(w1, w2) / (np.linalg.norm(w1) * np.linalg.norm(w2))
+        
+    def loadWordEmbeds(self, filename):
+        return gensim.models.KeyedVectors.load_word2vec_format(\
+                                    filename, binary=True)
+
+    def loadLabelEmbeds(self, filename):
+        """
+        Load the embeddings trained for each Label
+        """
+        return np.load(filename)
